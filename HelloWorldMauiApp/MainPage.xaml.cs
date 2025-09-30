6f20a2e6241c00787e2ad669ffe373d971b3830a
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-09-30 19:06:52 +0500

    Add project files.

---
+++ HelloWorldMauiApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace HelloWorldMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public bool IsLoggedIn { get; set; } = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LogButton.IsEnabled = false;
+
+            LogResultMessage.IsVisible = false;
+            UserNameEntry.IsVisible = true;
+        }
+
+        public void OnLogButtonClicked(object sender, EventArgs args)
+        {
+            if (IsLoggedIn)
+            {
+                UserNameEntry.Text = null;
+                UserNameEntry.IsVisible = true;
+
+                LogResultMessage.IsVisible = false;
+
+                LogButton.Text = "Log";
+            }
+            else
+            {
+                var username = UserNameEntry.Text.Trim();
+
+                LogResultMessage.Text = $"Hello, {username}";
+                LogResultMessage.IsVisible = true;
+
+                UserNameEntry.IsVisible = false;
+
+                LogButton.Text = "Retry";
+            }
+
+            IsLoggedIn = !IsLoggedIn;
+
+        }
+
+        public void OnUsernameEntryChanged(object sender, TextChangedEventArgs e)
+        {
+            LogButton.IsEnabled = !string.IsNullOrWhiteSpace(UserNameEntry.Text);
+        }
+    }
+}
